3dc6e99b61fdc16eea66f7994350e082c40ce2b5
evofan <evofan@outlook.jp>
2026-05-23 21:42:42 +0900

    add section-011 taiwayou-kansuu

---
+++ node-app/npx_ts-node_src_index_ts.bat
@@ -0,0 +1,2 @@
+
+cmd /k npx ts-node src/index.ts
--- node-app/src/index.ts
+++ node-app/src/index.ts
@@ -1,6 +1,26 @@
-const sayHello = (name:string) => {
-    return `Hello, ${name}`;
-}
+// const sayHello = (name:string) => {
+//     return `Hello, ${name}`;
+// }
 
-console.log(sayHello("Michael Jackson"));
+// console.log(sayHello("Michael Jackson"));
 // process.stdout.write(sayHello("Michael Jackson"));
+
+const printLine = (text: string, breakline: boolean = true) => {
+  console.log(text + (breakline ? "\n" : ""));
+};
+
+const promptInput = async (text: string) => {
+  printLine(`\n${text}\n> `, false);
+  const input: string = await new Promise((resolve) =>
+    process.stdin.once("data", (data) => resolve(data.toString())),
+  );
+  return input.trim();
+};
+
+(async () => {
+  const name = await promptInput("名前を入力して下さい");
+  console.log(name);
+  const age = await promptInput("年齢を入力して下さい");
+  console.log(age);
+  process.exit();
+})();

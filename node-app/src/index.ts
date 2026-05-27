23a2266d4b93e66df48fc6bf3bb8e5c359c93036
evofan <evofan@outlook.jp>
2026-05-27 20:18:08 +0900

    fix

--- node-app/src/index.ts
+++ node-app/src/index.ts
@@ -5,8 +5,9 @@
 // console.log(sayHello("Michael Jackson"));
 // process.stdout.write(sayHello("Michael Jackson"));
 
-const printLine = (text: string, breakline: boolean = true) => {
-  console.log(text + (breakline ? "\n" : ""));
+const printLine = (text: string, breakLine: boolean = true) => {
+  // console.log(text + (breakLine ? "\n" : ""));
+  process.stdout.write(text + (breakLine ? "\n" : ""));
 };
 
 const promptInput = async (text: string) => {
@@ -26,65 +27,64 @@ const promptInput = async (text: string) => {
 // })();
 
 class HitAndBlow {
-  private readonly answerSource = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  private answer: string[] = [];
-  private tryCount = 0;
-
-  // constructor() {
-  //   this.answerSource = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  //   this.answer = [];
-  //   this.tryCount = 0;
-  // }
+  private readonly answerSource = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+  private answer: string[] = []
+  private tryCount = 0
 
   setting() {
-    const answerLength = 3;
+    const answerLength = 3
 
     while (this.answer.length < answerLength) {
-      const randNum = Math.floor(Math.random() * this.answerSource.length);
-      const selectItem = this.answerSource[randNum];
-      if (!this.answer.includes(selectItem)) {
-        this.answer.push(selectItem);
+      const randNum = Math.floor(Math.random() * this.answerSource.length)
+      const selectedItem = this.answerSource[randNum]
+      if (!this.answer.includes(selectedItem)) {
+        this.answer.push(selectedItem)
       }
     }
   }
 
   async play() {
-    const inputArr = (
-      await promptInput(", 区切りで3つの数字を入力して下さい")
-    ).split(",");
-    const result = this.check(inputArr);
+    const inputArr = (await promptInput('「,」区切りで3つの数字を入力してください')).split(',')
+    const result = this.check(inputArr)
 
     if (result.hit !== this.answer.length) {
-      printLine(`---\nHit: ${result.hit}\nBlow: ${result.blow}\n---`);
-      this.tryCount += 1;
-      await this.play();
+      // 不正解だったら続ける
+      printLine(`---\nHit: ${result.hit}\nBlow: ${result.blow}\n---`)
+      this.tryCount += 1
+      await this.play()
     } else {
-      // game end
-      this.tryCount += 1;
+      // 正解だったら終了
+      this.tryCount += 1
     }
   }
 
+  end() {
+    printLine(`正解です！\n試行回数: ${this.tryCount}回`)
+    process.exit()
+  }
+
   private check(input: string[]) {
-    let hitCount = 0;
-    let blowCount = 0;
+    let hitCount = 0
+    let blowCount = 0
 
     input.forEach((val, index) => {
       if (val === this.answer[index]) {
-        hitCount + 1;
+        hitCount += 1
       } else if (this.answer.includes(val)) {
-        blowCount += 1;
+        blowCount += 1
       }
-    });
+    })
 
     return {
       hit: hitCount,
       blow: blowCount,
-    };
+    }
   }
 }
 
-(async () => {
-  const hitAndBlow = new HitAndBlow();
-  hitAndBlow.setting();
-  await hitAndBlow.play();
-})();
+;(async () => {
+  const hitAndBlow = new HitAndBlow()
+  hitAndBlow.setting()
+  await hitAndBlow.play()
+  hitAndBlow.end()
+})()

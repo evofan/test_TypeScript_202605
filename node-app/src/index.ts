9dd09f5ae3f8d9fb5b587b665925dbd296eaa00a
evofan <evofan@outlook.jp>
2026-06-02 21:32:29 +0900

    add GameProcedure class

--- node-app/src/index.ts
+++ node-app/src/index.ts
@@ -55,6 +55,28 @@ const promptSelect = async <T extends string>(
 const modes = ["normal", "hard"] as const;
 type Mode = (typeof modes)[number];
 
+class GameProcedure {
+  private currentGameTitle = "Hit and Blow";
+  private currentGame = new HitAndBlow();
+
+  public async start() {
+    await this.play();
+  }
+
+  private async play() {
+    printLine(`===\n${this.currentGameTitle} を開始します。\n===`);
+    await this.currentGame.setting();
+    await this.currentGame.play();
+    this.currentGame.end();
+    this.end();
+  }
+
+  private end() {
+    printLine("ゲームを終了しました。");
+    process.exit();
+  }
+}
+
 class HitAndBlow {
   private readonly answerSource = [
     "0",
@@ -126,7 +148,7 @@ class HitAndBlow {
 
   end() {
     printLine(`正解です！\n試行回数: ${this.tryCount}回`);
-    process.exit();
+    // process.exit();
   }
 
   private check(input: string[]) {
@@ -172,8 +194,11 @@ class HitAndBlow {
 }
 
 (async () => {
+  /*
   const hitAndBlow = new HitAndBlow();
   await hitAndBlow.setting();
   await hitAndBlow.play();
   hitAndBlow.end();
+  */
+ new GameProcedure().start();
 })();

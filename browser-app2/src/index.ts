b7f8907a0e858f402631fa2a7ec01f53a3a67397
evofan <evofan@outlook.jp>
2026-06-17 02:52:48 +0900

    add Application cass

--- browser-app2/src/index.ts
+++ browser-app2/src/index.ts
@@ -1,3 +1,14 @@
-import { sum } from "./sum";
+// import { sum } from "./sum";
 
-console.log(sum(1, 2));
+// console.log(sum(1, 2));
+
+class Applicatio {
+    start(){
+        console.log("hello world");
+    }
+}
+
+window.addEventListener("load", ()=>{
+    const app = new Applicatio();
+    app.start();
+})

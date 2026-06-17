c6dc1537661b7989c446d23532833bce1cf167d9
evofan <evofan@outlook.jp>
2026-06-17 09:16:52 +0900

    add Task class

--- browser-app2/src/index.ts
+++ browser-app2/src/index.ts
@@ -3,29 +3,51 @@
 // console.log(sum(1, 2));
 
 import { EventLListener } from "./EventListeners";
+import { Task } from "./Task";
 
-class Applicatio {
+class Application {
+  private readonly eventListener = new EventLListener();
+
   start() {
     console.log("hello world");
 
-    const eventListener = new EventLListener();
+    // const eventListener = new EventLListener();
+    // const button = document.getElementById("deleteAllDoneTask");
 
-    const button = document.getElementById("deleteAllDoneTask");
+    // if (!button) {
+    //   return;
+    // }
 
-    if (!button) {
+    // console.log(button);
+    // eventListener.add("sample", "click", button, () => alert("clicked"));
+    // eventListener.remove("sample");
+
+    const createForm = document.getElementById("createForm") as HTMLElement;
+
+    this.eventListener.add(
+      "submit-handler",
+      "submit",
+      createForm,
+      this.handleSubmit,
+    );
+  }
+
+  private handleSubmit = (e: Event) => {
+    e.preventDefault();
+    console.log("submited !");
+
+    const titleInput = document.getElementById("title") as HTMLInputElement;
+
+    if (!titleInput.value) {
       return;
     }
 
-    console.log(button);
-
-    eventListener.add("sample", "click", button, () => alert("clicked"));
-
-    eventListener.remove("sample");
-  }
-
+    const task = new Task({ title: titleInput.value });
+    console.log(task);
+  };
 }
 
 window.addEventListener("load", () => {
-  const app = new Applicatio();
+  const app = new Application();
   app.start();
 });

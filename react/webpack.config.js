d931656600d7b431d4f2f0426b544fb95e737bb3
evofan <evofan@outlook.jp>
2026-06-24 06:07:06 +0900

    add react file

---
+++ react/webpack.config.js
@@ -0,0 +1,25 @@
+const { resolve } = require("path");
+
+module.exports = {
+  mode: "development",
+  // mode: process.env.NODE_ENV || "development",
+  devtool: "inline-source-map",
+  entry: resolve(__dirname, "src/index.jsx"),
+  output: {
+    filename: "index.js",
+    path: resolve(__dirname, "dist"),
+  },
+  resolve: {
+    extensions: [".ts", ".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|jsx)/,
+        use: {
+          loader: "ts-loader",
+        },
+      },
+    ],
+  },
+};

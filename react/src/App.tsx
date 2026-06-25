dde85bd44cb59d8d1f7a32608af6ce3dafe4904b
evofan <evofan@outlook.jp>
2026-06-25 21:37:44 +0900

    .jsx -> .tsx

---
+++ react/src/App.tsx
@@ -0,0 +1,8 @@
+import { Text } from "./libs/Text"
+
+export const App = () => {
+    return (
+       // <h1>React App</h1>
+        <Text text="Hello, World!" />
+    )
+}
--- react/webpack.config.js
+++ react/webpack.config.js
@@ -4,18 +4,18 @@ module.exports = {
   mode: "development",
   // mode: process.env.NODE_ENV || "development",
   devtool: "inline-source-map",
-  entry: resolve(__dirname, "src/index.jsx"),
+  entry: resolve(__dirname, "src/index.tsx"),
   output: {
     filename: "index.js",
     path: resolve(__dirname, "dist"),
   },
   resolve: {
-    extensions: [".ts", ".js", ".jsx"],
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
   },
   module: {
     rules: [
       {
-        test: /\.(ts|jsx)/,
+        test: /\.(ts|tsx|jsx)$/,
         use: {
           loader: "ts-loader",
         },

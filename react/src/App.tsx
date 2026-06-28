f5894c679bc6a7f844ae49f7867a01448952d9ed
evofan <evofan@outlook.jp>
2026-06-28 22:43:30 +0900

    add styled-components

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -2,7 +2,7 @@ import { Text } from "./libs/Text"
 
 export const App = () => {
     return (
-       // <h1>React App</h1>
-        <Text text="Hello, World!" />
+        // <h1>React App</h1>
+        <Text text="true" />
     )
 }

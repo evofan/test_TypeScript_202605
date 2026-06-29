fde54d3f91df4c94e1106eb52777da9a6520ba9a
evofan <evofan@outlook.jp>
2026-06-29 22:39:14 +0900

    add Heading

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,8 +1,16 @@
 import { Text } from "./libs/Text"
+import { Heading } from "./libs/Heading"
 
 export const App = () => {
     return (
         // <h1>React App</h1>
-        <Text text="true" />
+
+        <>
+            <Text text="true" />
+            <Heading tag="h1">見出し</Heading>
+            <Heading tag="h1">
+                <span>hello, world</span>
+            </Heading>
+        </>
     )
 }

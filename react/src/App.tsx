61994bd28b584d22736e791768112dae1ff757e5
evofan <evofan@outlook.jp>
2026-07-02 19:24:29 +0900

    fix Button.tsx

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,5 +1,6 @@
 import { Text } from "./libs/Text"
 import { Heading } from "./libs/Heading"
+import { Button } from "./libs/Button"
 
 export const App = () => {
     return (
@@ -7,10 +8,14 @@ export const App = () => {
 
         <>
             <Text text="true" />
-            <Heading tag="h1">見出し</Heading>
+            <Heading tag="h1">見出し</Heading>z
             <Heading tag="h1">
                 <span>hello, world</span>
             </Heading>
+
+            <Button onClick={() => console.log("clicked")} title="Button" type="primary" width={96} />
+            <Button onClick={() => console.warn("clicked")} title="Button" type="secondary" />
+            <Button onClick={() => console.error("clicked")} title="Button" type="error" />
         </>
     )
 }
--- react/src/libs/Button.tsx
+++ react/src/libs/Button.tsx
@@ -1,4 +1,6 @@
 import { VFC } from "react";
+import styled, { css } from "styled-components";
+import { color, radius, space } from "./constants";
 
 type ButtonType = "primary" | "secondary" | "error";
 
@@ -6,16 +8,49 @@ type Props = {
     title: string,
     onClick: () => void,
     type?: ButtonType,
-    widt?: number
+    width?: number,
 }
+
 
 export const Button: VFC<Props> = ({
     title,
     onClick,
+    width = 80,
+    type = "primary"
 }) => {
     return (
-        <button onClick={onClick}>
+        // <button onClick={onClick}>
+        //     {title}
+        // </button>
+        <Wrapper onClick={onClick} width={width} className={type}>
             {title}
-        </button>
+        </Wrapper>
     )
 }
+
+const Wrapper = styled.button<{ width: number }>
+    `padding: ${space.m}
+border-radius: ${radius.m};
+border:solid 1px ${color.green};
+background: ${color.green};
+color:${color.white};
+text-align:center;
+cursor:pointer;
+box-sizing:border-box:
+
+${props => css`width: ${props.width}px`}
+
+&.secondary {
+    border:solid 1px ${color.gray};
+background: ${color.white};
+color:${color.black};
+}
+
+&.error {
+    border:none;
+background: ${color.red};
+color:${color.white};
+}
+`;
+
+
